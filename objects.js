00d3fa8679218f8cec2cb23ce5d7008ec3386ee1
Ramsey Chapin <ramsey@ramseychapin.com>
2019-02-24 23:54:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,7 @@ function updatePlaylist (playlist, artist, song) {
   return playlist;
 }
 
-function removeFromPlaylist (playlist, artist, song) {
-  
+function removeFromPlaylist (playlist, artist) {
+  delete playlist.artist;
+  return playlist;
 }
