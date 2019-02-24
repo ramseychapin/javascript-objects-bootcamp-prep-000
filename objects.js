42aca573631924e7abf89c7dde03765425f4c9d0
Ramsey Chapin <ramsey@ramseychapin.com>
2019-02-24 23:53:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,4 +2,9 @@ var playlist = new Object ({Prince: "Purple Rain"})
 
 function updatePlaylist (playlist, artist, song) {
   playlist[playlist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist (playlist, artist, song) {
+  
 }
